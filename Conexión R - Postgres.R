0a7a0c2b22a28b6522ef0c38beca110cedba0c08
Ludsss <ludovica.verrieri@mail.polimi.it>
2019-06-05 17:49:44 +0200

    Update Conexión R - Postgres.R

--- Conexión R - Postgres.R
+++ Conexión R - Postgres.R
@@ -22,3 +22,20 @@ dbExistsTable(con, "desc_marca_vehiculo")
 # Extraer datos de la base de datos
 datos_postgres <- dbGetQuery(con, "SELECT * FROM desc_marca_vehiculo")
 datos_postgres
+
+
+# PARA CONECTAR A LA BBDD DE LUDO
+
+# Guardar el Password para poder posteriormente eliminarlo
+pw <- { "ludo"}
+
+# Leer el driver de PostgreSQL
+drv <- dbDriver("PostgreSQL")
+
+# Crear la conexion con la base de datos
+con <- dbConnect(drv, dbname = "accidentes_barcelona",
+                 host = "192.168.0.165", port = 5432,
+                 user = "postgres", password = pw)
+
+# Eliminar el Password
+rm(pw) 
